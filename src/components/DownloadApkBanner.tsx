dd0b6b19c787e7415fc91b94e87a3b23f6824c3c
Pedro Henrique Freitas Ferreira <phfreitasf@gmail.com>
2022-09-27 13:59:14 -0300

    link download apk

--- src/components/DownloadApkBanner.tsx
+++ src/components/DownloadApkBanner.tsx
@@ -6,7 +6,7 @@ export function DownloadApkBanner() {
         <div className='bg-[#2A2634] px-8 py-6 flex flex-col justify-center items-center'>
 
           <strong className='text-md sm:text-2xl text-white font-black block mb-1'>Aplicativo Android</strong>
-            <a href={'https://drive.google.com/file/d/15CMW4aI5W_os7YjknZbAEgz2W0OSCIUn/view?usp=sharing'} target='_blank'>
+            <a href={'https://drive.google.com/file/d/1V3QZCQtE7iOY98BPssPCcxWlohOJGqU9/view?usp=sharing'} target='_blank'>
             <button className='py-3 px-4 bg-sky-700 text-white items-center rounded hover:bg-sky-900 flex gap-3 text-lg'><AndroidLogo size={32} />Download</button>
             </a>
         </div>
